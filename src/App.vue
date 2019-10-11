c58c50a078e92d9195c07f9a13bce1ecb1f66931
Kevin McDermott <kmcdermott31@gmail.com>
2019-10-11 13:50:15 +0100

    initial styling

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
     <div class="header">
       <h1>The Breaking Bad Compendium</h1>
     </div>
+    <div class="random-quote">
+      <random-quote :randomQuote="randomQuote"></random-quote>
+    </div>
+    <br>
     <div class="data-container">
       <div class="episode-container">
         <div class="dropdown">
-          <sub>Choose an episode to find out more about it</sub>
+          <h2>Episode Chooser</h2>
           <episode-select :episodes="episodes"></episode-select>
         </div>
         <div class="info-panel">
@@ -15,7 +19,7 @@
       </div>
       <div class="character-container">
         <div class="dropdown">
-          <sub>Choose a character to find out more about them</sub>
+          <h2>Character Chooser</h2>
           <character-select :characters="characters"></character-select>
         </div>
         <div class="info-panel">
@@ -32,6 +36,7 @@ import EpisodeSelect from './components/EpisodeSelect.vue';
 import EpisodeDetail from './components/EpisodeDetail.vue';
 import CharacterSelect from './components/CharacterSelect.vue';
 import CharacterDetails from './components/CharacterDetail.vue';
+import RandomQuote from './components/RandomQuote.vue';
 
 export default {
   name: 'app',
@@ -74,19 +79,46 @@ export default {
     "episode-select": EpisodeSelect,
     "episode-details": EpisodeDetail,
     "character-select": CharacterSelect,
-    "character-details": CharacterDetails
+    "character-details": CharacterDetails,
+    "random-quote": RandomQuote
   }
 }
 </script>
 
 <style lang="css" scoped>
-h1 {
+.header {
+  background: url('https://coverfiles.alphacoders.com/753/75395.jpg') 100% 100% no-repeat;
+  padding: 5rem 5rem;
+  margin-left: -8rem;
+  width: 100vmax;
+  height: 15vmax;
+  border-bottom: 3px solid white;
+}
+
+.header h1 {
   text-align: center;
+  font-size: 72px;
+  font-family: helvetica;
+  color: lightgrey;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: lawnGreen;
 }
 
 .data-container {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+}
+
+.episode-container {
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 0 10rem 2rem;
+}
+
+.character-container {
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 0 10rem 2rem;
 }
 </style>
---
+++ src/components/RandomQuote.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="quotation">
+    <sub v-for="quote in randomQuote">
+      <q>{{ quote.quote }}</q> - {{ quote.author }}
+    </sub>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'random-quote',
+  props: ['randomQuote']
+}
+</script>
+
+<style lang="css" scoped>
+.quotation {
+  text-align: center;
+  margin-top: 0.5rem;
+}
+
+sub {
+  padding: 0.4rem;
+  border: 1px dotted black;
+  border-radius: 25px;
+}
+</style>
